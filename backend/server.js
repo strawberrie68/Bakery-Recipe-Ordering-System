5da3e55f9d600969a4df51dfda417727a1bd9cc9
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-07-25 15:11:24 -0700

    order of port

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,17 @@ const port = process.env.PORT || 6012;
 app.use(cors());
 app.use(express.json());
 
-
-connectDB()
-
 //  routes
 app.use('/recipe', recipeRouter);
 app.use('/supplier', supplierRouter);
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`)
+    })
 })
+
+
+
+
